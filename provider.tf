7bbf4963866d9d55863757e8ef590943456112a6
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-06-18 09:06:32 +0200

    🔧 : add provider configuration

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.2.1"
+    }
+  }
+  required_version = "> 1.8"
+}
+
+provider "github" {
+  owner = var.owner
+}
